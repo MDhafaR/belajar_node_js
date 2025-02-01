0708578e433a3ff6507fb567804153054546b449
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2025-02-01 09:35:44 +0700

    node fs

--- 8-node-app/contacs.js
+++ 8-node-app/contacs.js
@@ -34,39 +34,128 @@ if (!fs.existsSync(filePath)) {
 //   rl.close();
 // };
 
+const bacaData = function () {
+  const fileValue = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+  return fileValue;
+};
+
 const isiPertanyaan = function (nama, email, nohp) {
   let valueContact = { nama: nama, email: email, nohp: nohp };
 
-  const fileValue = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-  fileValue.push(valueContact);
-  fs.writeFileSync(filePath, JSON.stringify(fileValue));
+  const data = bacaData();
+
+  data.push(valueContact);
+  fs.writeFileSync(filePath, JSON.stringify(data));
+};
+
+const listContacs = function () {
+  const data = bacaData();
+  console.log("Daftar Nama :");
+
+  data.forEach((contact, i) => {
+    console.log(`${i + 1}. nama: ${contact.nama}, nomor: ${contact.nohp}`);
+  });
+};
+
+const detailContact = function (nama) {
+  const data = bacaData();
+  const contact = data.find(
+    (contact) => contact.nama.toLowerCase() === nama.toLowerCase()
+  );
+  if (!contact) {
+    console.log("tidak ada nama dalam daftar");
+  } else {
+    console.log(`nama  : ${contact.nama}`);
+    if (contact.email) {
+      console.log(`email : ${contact.email}`);
+    }
+    console.log(`nomor : ${contact.nohp}`);
+  }
+};
+
+const deleteContact = function (nama) {
+  const data = bacaData();
+
+  const newData = data.filter(
+    (contact) => contact.nama.toLowerCase() !== nama.toLowerCase()
+  );
+
+  if (newData.length === data.length) {
+    console.log(`Kontak ${nama} tidak ditemukan.`);
+  } else {
+    console.log(`Kontak ${nama} berhasil dihapus.`);
+
+    newData.forEach((contact, i) => {
+      console.log(`${i + 1}. nama: ${contact.nama}, nomor: ${contact.nohp}`);
+    });
+  }
 };
 
 const yargsFun = function () {
-  yargs.command(
-    "add",
-    "untuk menambahkan data",
-    {
+  yargs
+    .command(
+      "add",
+      "untuk menambahkan data",
+      {
+        nama: {
+          describe: "digunakan untuk menginputkan nama",
+          type: "string",
+          demandOption: true,
+        },
+        email: {
+          describe: "digunakan untuk menginputkan email",
+          type: "string",
+          demandOption: false,
+        },
+        nohp: {
+          describe: "digunakan untuk menginputkan nomor hp",
+          type: "string",
+          demandOption: false,
+        },
+      },
+      function (argv) {
+        isiPertanyaan(argv.nama, argv.email, argv.nohp);
+      }
+    )
+    .demandCommand();
+
+  yargs.command({
+    command: "list",
+    describe: "untuk melihat seluruh nama yang terdaftar",
+    handler() {
+      listContacs();
+    },
+  });
+
+  yargs.command({
+    command: "detail",
+    describe: "mencari detail data dari nama lengkap yang terdaftar",
+    builder: {
       nama: {
-        describe: "digunakan untuk menginputkan nama",
+        describe: "digunakan untuk menginputkan nama lengkap",
         type: "string",
         demandOption: true,
       },
-      email: {
-        describe: "digunakan untuk menginputkan email",
+    },
+    handler(argv) {
+      detailContact(argv.nama);
+    },
+  });
+
+  yargs.command({
+    command: "delete",
+    describe: "mengapus data menggunakan nama lengkap",
+    builder: {
+      nama: {
+        describe: "digunakan untuk menginputkan nama lengkap",
         type: "string",
-        demandOption: false,
-      },
-      nohp: {
-        describe: "digunakan untuk menginputkan nomor hp",
-        type: "string",
-        demandOption: false,
+        demandOption: true,
       },
     },
-    function (argv) {
-      isiPertanyaan(argv.nama, argv.email, argv.nohp);
-    }
-  );
+    handler(argv) {
+      deleteContact(argv.nama);
+    },
+  });
 
   yargs.parse();
 };
