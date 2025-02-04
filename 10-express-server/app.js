6201dd809489e557ebbb6ea45691919c2794d256
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2025-02-04 14:53:02 +0700

    init for npm

--- 10-express-server/app.js
+++
@@ -1,7 +0,0 @@
-const server = require('./web_server');
-
-const main = function(){
-    server.server();
-}
-
-main();
--- 6-module-system/file_satu.js
+++
@@ -1,9 +0,0 @@
-const fileDua = require('./folder_file_dua/file_dua');
-
-const namaSaya = 'Muhammad Dhafa Ramadhani';
-const namaKliju = 'Anisa';
-const tahunLahirSaya = 2004;
-const tahunLahirKliju = 2005;
-
-fileDua.mixName(namaSaya, namaKliju);
-fileDua.mixUmur(tahunLahirSaya, tahunLahirKliju);
